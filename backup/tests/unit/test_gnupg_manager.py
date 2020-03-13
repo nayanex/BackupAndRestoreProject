616d7c89603560b74a477d406d99b7e9d1715dc1
Nayanex <holanda.nayana@gmail.com>
2020-03-13 20:19:43 +0100

    Update test_gnupg_manager.py

--- backup/tests/unit/test_gnupg_manager.py
+++ backup/tests/unit/test_gnupg_manager.py
@@ -1,13 +1,3 @@
-##############################################################################
-# COPYRIGHT Ericsson AB 2018
-#
-# The copyright to the computer program(s) herein is the property of
-# Ericsson AB. The programs may be used and/or copied only with written
-# permission from Ericsson AB. or in accordance with the terms and
-# conditions stipulated in the agreement/contract under which the
-# program(s) have been supplied.
-##############################################################################
-
 #pylint: disable=E0401
 #pylint: disable=C0103
 
