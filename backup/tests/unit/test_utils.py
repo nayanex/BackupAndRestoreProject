e1788d02d03b55ddb3657c82290434d9c810ea3a
Nayanex <holanda.nayana@gmail.com>
2020-03-23 18:40:59 +0100

    Update test_utils.py

--- backup/tests/unit/test_utils.py
+++ backup/tests/unit/test_utils.py
@@ -1,13 +1,3 @@
-##############################################################################
-# COPYRIGHT Ericsson 2018
-#
-# The copyright to the computer program(s) herein is the property of
-# Ericsson Inc. The programs may be used and/or copied only with written
-# permission from Ericsson Inc. or in accordance with the terms and
-# conditions stipulated in the agreement/contract under which the
-# program(s) have been supplied.
-##############################################################################
-
 # For the snake_case comments
 # pylint: disable=C0103
 
