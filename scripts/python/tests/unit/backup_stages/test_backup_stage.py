e57465c752457956922c28a603291fe73ba06d45
Nayanex <holanda.nayana@gmail.com>
2020-03-30 23:05:28 +0200

    Update test_backup_stage.py

--- scripts/python/tests/unit/backup_stages/test_backup_stage.py
+++ scripts/python/tests/unit/backup_stages/test_backup_stage.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-##############################################################################
-# COPYRIGHT Ericsson AB 2018
-#
-# The copyright to the computer program(s) herein is the property of
-# Ericsson AB. The programs may be used and/or copied only with written
-# permission from Ericsson AB. or in accordance with the terms and
-# conditions stipulated in the agreement/contract under which the
-# program(s) have been supplied.
-##############################################################################
-
 """
 This module is for unit testing of the no_storage_wfs method from
 scripts.python.backup_scheduler.backup_handlers.BackupStages class
