d6d80e450cb4a3e5b762804afa61b0e571a0a8d1
Nayanex <holanda.nayana@gmail.com>
2020-05-27 22:45:01 +0200

    Update backup_utils.py

--- scripts/python/backup_scheduler/backup_utils.py
+++ scripts/python/backup_scheduler/backup_utils.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python
-##############################################################################
-# COPYRIGHT Ericsson AB 2018
-#
-# The copyright to the computer program(s) herein is the property of
-# Ericsson AB. The programs may be used and/or copied only with written
-# permission from Ericsson AB. or in accordance with the terms and
-# conditions stipulated in the agreement/contract under which the
-# program(s) have been supplied.
-##############################################################################
 
 """
 Utils script for common use
