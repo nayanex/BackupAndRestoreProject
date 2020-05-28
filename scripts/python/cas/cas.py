a73b9a84adec43a5d6de22ac95e24193255731fa
Nayanex <holanda.nayana@gmail.com>
2020-05-28 23:47:14 +0200

    Update cas.py

--- scripts/python/cas/cas.py
+++ scripts/python/cas/cas.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python3
 
-##############################################################################
-# COPYRIGHT Ericsson 2018
-#
-# The copyright to the computer program(s) herein is the property of
-# Ericsson Inc. The programs may be used and/or copied only with written
-# permission from Ericsson Inc. or in accordance with the terms and
-# conditions stipulated in the agreement/contract under which the
-# program(s) have been supplied.
-##############################################################################
-
 import argparse
 import logging
 import sys
